8681bcc67414c101a8afd986516eee20f072e317
kah-rizza <bu95509@gmail.com>
2023-06-13 22:30:05 +0600

    программа, которая принимает на вход число и выдаёт сумму цифр в числе

--- Zadanie_27/Program.cs
+++ Zadanie_27/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        string input = Console.ReadLine();
+
+        // Валидация ввода
+        if (!IsValidNumber(input))
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+            return;
+        }
+
+        int number = int.Parse(input);
+
+        // Вычисление суммы цифр
+        int sum = GetDigitSum(number);
+
+        Console.WriteLine("Сумма цифр в числе: " + sum);
+        Console.ReadLine();
+    }
+
+    // Проверка валидности числа
+    static bool IsValidNumber(string input)
+    {
+        int number;
+        return int.TryParse(input, out number);
+    }
+
+    // Вычисление суммы цифр в числе
+    static int GetDigitSum(int number)
+    {
+        int sum = 0;
+
+        while (number != 0)
+        {
+            int digit = number % 10; // Получаем последнюю цифру числа
+            sum += digit; // Добавляем цифру к сумме
+            number /= 10; // Удаляем последнюю цифру числа
+        }
+
+        return sum;
+    }
+}
