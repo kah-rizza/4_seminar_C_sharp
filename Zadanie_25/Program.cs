825ddc13d78377b596908b9123f45bf7b1b3cef8
kah-rizza <bu95509@gmail.com>
2023-06-13 22:21:30 +0600

    цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B

--- Zadanie_25/Program.cs
+++ Zadanie_25/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число A:");
+        double A;
+        if (!double.TryParse(Console.ReadLine(), out A))
+        {
+            Console.WriteLine("Некорректный ввод числа A. Пожалуйста, введите число.");
+            return;
+        }
+
+        Console.WriteLine("Введите число B:");
+        int B;
+        if (!int.TryParse(Console.ReadLine(), out B) || B < 0)
+        {
+            Console.WriteLine("Некорректный ввод числа B. Пожалуйста, введите неотрицательное целое число.");
+            return;
+        }
+
+        double result = 1;
+
+        for (int i = 1; i <= B; i++)
+        {
+            result *= A;
+        }
+
+        Console.WriteLine("Результат: " + result);
+        Console.ReadLine();
+    }
+}
